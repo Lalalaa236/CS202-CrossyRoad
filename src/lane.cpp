9d7d15e2852f61e54932435b1cb99fd2970c78a1
apple2712 <caotin27122003@gmail.com>
2023-11-23 22:35:16 +0700

    debug lane

--- src/lane.cpp
+++ src/lane.cpp
@@ -1,38 +1,24 @@
-// #include "lane.h"
-// #include<string>
+#include "lane.h"
 
-// using namespace std;
-// #define SIZE_LANE 200
+using namespace std;
 
-// Lane::Lane(float y, int id, string obstacle) {
-//     this->y= SIZE_LANE;
-//     this->id= 0;
-//     this->obstacles = obstacles;
+Lane::Lane(float y, int id) {
+    this->y= 100;
+    this->id= 0;
+}
 
-// }
+void Lane::addObstacle(Obstacle* obstacle) {
+    obstacles.push_back(obstacle);
+}
 
-// void Lane::addObstacle(Obstacle* obstacle) {
-//     obstacle.pushback();
-// }
+void Lane::draw() {
 
-// void Lane::draw() {
-//     for(int i=0; i< obstacle.size(); i++) {
-//         if() {
-//             // Draw the obstacle
-//         }
-//     }
-//     // Random lane
-//     for(int i=0; i<3; i++) {
-//         if(true) {
-//             // Draw lane
-//         }
-//     }
-// }
+}
 
-// Lane::~Lane() {
-//     for(int i=0;i< obstacle.size(); i++) {
-//         delete obstacle[i];
-//     }
-//     delete []trafficLight;
-// }
+Lane::~Lane() {
+    while(obstacles.size()) {
+        delete obstacles.front();
+        obstacles.pop_front();
+    }
+}
 
--- src/lane.h
+++ src/lane.h
@@ -1,25 +1,24 @@
-// #ifndef LANE_H
-// #define LANE_H
+#ifndef LANE_H
+#define LANE_H
 
-// #include "Obstacle.h"
-// #include "TrafficLight.h"
-// #include "TrafficLight.h"
-// #include <deque>
+#include "Obstacle.h"
+#include "TrafficLight.h"
+#include <deque>
 
-// class Lane {
-//     public:
-//         Lane(float y, int id);
-//         void addObstacle(Obstacle* obstacle);
-//         void draw();
-//         ~Lane();
+using namespace std;
 
-//     private:
-//         float y;
-//         int id;
-//         deque <Obstacle*> obstacles;
-//         TrafficLight* trafficLight;
-//         string nameObstacle;
-//         string nameLane[3];
-// };
+class Lane {
+    public:
+        Lane(float y, int id);
+        void addObstacle(Obstacle* obstacle);
+        void draw();
+        ~Lane();
 
-// #endif
+    private:
+        float y;
+        int id;
+        deque <Obstacle*> obstacles;
+        TrafficLight* trafficLight;
+};
+
+#endif
