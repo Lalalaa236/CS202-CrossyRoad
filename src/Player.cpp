9cde04bbebe28f36c112dd1e311c79e149e403ab
Lalalaa236 <nhankiet236@gmail.com>
2023-11-26 00:43:50 +0700

    Added update and box collision for Player

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,8 +1,13 @@
 #include "Player.h"
 
-Player::Player(float x, float y)
-: position({x, y}), isAlive(true)
-{}
+Player::Player(float x, float y, float mapSpeed)
+: position({x, y}), isAlive(true), mapSpeed(mapSpeed)
+{
+    boxCollision.x = x;
+    boxCollision.y = y;
+    boxCollision.width = settings::GRID_SIZE.first;
+    boxCollision.height = settings::GRID_SIZE.second;
+}
 
 Player::Player(float x, float y, bool isAlive)
 : position({x, y}), isAlive(isAlive)
@@ -11,21 +16,25 @@ Player::Player(float x, float y, bool isAlive)
 void Player::up()
 {
     position.second -= settings::GRID_SIZE.second;
+    boxCollision.y -= settings::GRID_SIZE.second;
 }
 
 void Player::down()
 {
     position.second += settings::GRID_SIZE.second;
+    boxCollision.y += settings::GRID_SIZE.second;
 }
 
 void Player::left()
 {
     position.first -= settings::GRID_SIZE.first;
+    boxCollision.x -= settings::GRID_SIZE.first;
 }
 
 void Player::right()
 {
     position.first += settings::GRID_SIZE.first;
+    boxCollision.x += settings::GRID_SIZE.first;
 }
 
 std::pair<float, float> Player::getPosition()
@@ -55,4 +64,30 @@ void Player::move(Direction direction)
 bool Player::getIsAlive()
 {
     return isAlive;
+}
+
+void Player::setIsAlive(bool isAlive)
+{
+    this->isAlive = isAlive;
+}
+
+Rectangle Player::getBoxCollision()
+{
+    return boxCollision;
+}
+
+void Player::update()
+{
+    position.second += mapSpeed;
+    boxCollision.y += mapSpeed;
+}
+
+void Player::setMapSpeed(float mapSpeed)
+{
+    this->mapSpeed = mapSpeed;
+}
+
+void Player::draw()
+{
+    DrawRectanglePro(boxCollision, {position.first, position.second}, 0, RED);
 }
--- src/Player.h
+++ src/Player.h
@@ -2,16 +2,19 @@
 #define PLAYER_H
 
 #include "GameSettings.h"
+#include "TextureHolder.h"
 
 class Player
 {
     private:
         std::pair<float, float> position;
+        float mapSpeed;
         bool isAlive;
         void up();
         void down();
         void left();
         void right();
+        Rectangle boxCollision;
     public:
         enum class Direction
         {
@@ -20,11 +23,18 @@ class Player
             LEFT,
             RIGHT
         };
-        Player(float x, float y);
+        Player(float x, float y, float mapSpeed);
         Player(float x, float y, bool isAlive);
+        
         std::pair<float, float> getPosition();
-        void move(Direction direction);
         bool getIsAlive();
+        void setIsAlive(bool isAlive);
+        void setMapSpeed(float mapSpeed);
+        Rectangle getBoxCollision();
+
+        void move(Direction direction);
+        void update();
+        void draw();
 };
 
 #endif
