484b520af3eeeb1060ed0095e9baeb6df537292a
Lalalaa236 <nhankiet236@gmail.com>
2023-12-10 01:00:17 +0700

    Added MusicManager class

---
+++ src/MusicManager.cpp
@@ -0,0 +1,63 @@
+#include "MusicManager.h"
+
+MusicManager::MusicManager()
+{
+    isSoundOn = true;
+    volume = 1.0f;
+
+    InitAudioDevice();
+    music = LoadMusicStream("image/Sound/whistle.mp3");
+    SetMusicVolume(music, volume);
+    PlayMusicStream(music);
+}
+
+MusicManager::~MusicManager()
+{
+    UnloadMusicStream(music);
+    CloseAudioDevice();
+}
+
+MusicManager& MusicManager::getManager()
+{
+    static MusicManager manager;
+    return manager;
+}
+
+void MusicManager::play()
+{
+    if(isSoundOn)
+        UpdateMusicStream(music);
+}
+
+float MusicManager::getVolume() const
+{
+    return volume;
+}
+
+void MusicManager::setMusic(const std::string& path)
+{
+    UnloadMusicStream(music);
+    music = LoadMusicStream(path.c_str());
+    SetMusicVolume(music, volume);
+    PlayMusicStream(music);
+}
+
+bool MusicManager::getIsSoundOn() const
+{
+    return isSoundOn;
+}
+
+void MusicManager::toggleSound()
+{
+    isSoundOn = !isSoundOn;
+    if(isSoundOn)
+        PlayMusicStream(music);
+    else
+        StopMusicStream(music);
+}
+
+void MusicManager::setVolume(float volume)
+{
+    this->volume = volume;
+    SetMusicVolume(music, volume);
+}
